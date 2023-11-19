11f71cb15affd8ddfa88d787cacefb23f1a5d0af
Evmorfia Elpida Dasyra <dasyevita@gmail.com>
2023-11-19 17:26:49 +0200

    Initial commit,add basic structure of project

---
+++ app/src/main/java/com/example/bookify_frontend/ui/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bookify_frontend.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.bookify_frontend.R
+import com.example.bookify_frontend.ui.home.HomeActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+//        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
