43b843b38207df54aeccf3c4a1fe10de86d54ed0
Evmorfia Elpida Dasyra <dasyevita@gmail.com>
2023-11-19 20:30:20 +0200

    update gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,4 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
-
 }
