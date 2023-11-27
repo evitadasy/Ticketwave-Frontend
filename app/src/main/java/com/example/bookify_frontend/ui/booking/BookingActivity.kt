ff0d90b3e04607b36cd3312464860f111f7170dd
Evmorfia Elpida Dasyra <dasyevita@gmail.com>
2023-11-27 20:03:38 +0200

    change fragments to acitivities (fragments will be deleted later)

---
+++ app/src/main/java/com/example/bookify_frontend/ui/booking/BookingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookify_frontend.ui.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookify_frontend.R
+
+class BookingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking)
+    }
+}
---
+++ app/src/main/java/com/example/bookify_frontend/ui/category/CategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookify_frontend.ui.category
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookify_frontend.R
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
