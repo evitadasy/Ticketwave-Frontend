ccb332001fc17a29af7c49af37d17ea19bea60fb
Kleonhs <148132975+Kleonhs@users.noreply.github.com>
2023-12-01 17:42:24 +0200

    Scroll View XML

--- app/src/main/java/com/example/bookify_frontend/ui/booking/BookingActivity.kt
+++ app/src/main/java/com/example/bookify_frontend/ui/booking/BookingActivity.kt
@@ -1,55 +1,26 @@
-package com.example.bookify_frontend.ui.booking
-
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bookify_frontend.R
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+// Class definition
+class BookingActivity : AppCompatActivity() {
 
-class BookingFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
+    // onCreate method
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_booking, container, false)
+        // Set the content view to the layout defined in activity_booking.xml
+        setContentView(R.layout.activity_booking)
 
         // Find the "Book Now" button in the layout
-        val bookNowButton: Button = rootView.findViewById(R.id.button3)
+        val bookNowButton: Button = findViewById(R.id.button3)
 
         // Set a click listener for the button
         bookNowButton.setOnClickListener {
             // Code to be executed when the "Book Now" button is clicked
-            Toast.makeText(requireContext(), "Book Now Button Clicked!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Book Now Button Clicked!", Toast.LENGTH_SHORT).show()
         }
-
-        return rootView
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BookingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
