69eea1ce0525ec0137c3fa68eaa5597c1df2b8dd
Evmorfia Elpida Dasyra <dasyevita@gmail.com>
2023-12-04 19:33:19 +0200

    add ConfirmationActivity

---
+++ app/src/main/java/com/example/bookify_frontend/ui/confirmation/ConfirmationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookify_frontend.ui.confirmation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookify_frontend.R
+
+class ConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation)
+    }
+}
