3ac726951736c86d54026070db4b62bbe6afc9a7
Evmorfia Elpida Dasyra <dasyevita@gmail.com>
2023-12-04 23:08:33 +0200

    create ConfirmActivity

--- app/src/main/java/com/example/bookify_frontend/ui/confirmation/ConfirmationActivity.kt
+++ app/src/main/java/com/example/bookify_frontend/ui/confirmation/ConfirmationActivity.kt
@@ -1,12 +1,38 @@
 package com.example.bookify_frontend.ui.confirmation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.bookify_frontend.R
+import com.example.bookify_frontend.ui.category.CategoryActivity
+import com.example.bookify_frontend.ui.home.HomeActivity
 
 class ConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmation)
+
+        val btnExit: Button = findViewById(R.id.btnExit)
+        btnExit.setOnClickListener {
+            navigateToHomeActivity()
+        }
+
+        val btnContinue: Button = findViewById(R.id.btnContinue)
+        btnContinue.setOnClickListener {
+            navigateToCategoryActivity()
+        }
+    }
+
+    private fun navigateToHomeActivity() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun navigateToCategoryActivity() {
+        val intent = Intent(this, CategoryActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
